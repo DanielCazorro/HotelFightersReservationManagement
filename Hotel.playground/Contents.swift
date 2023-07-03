dc8c10364ac201405d792b7a86f77f655fc84343
Daniel Cazorro <leinadhunter10@hotmail.com>
2023-07-03 18:00:48 +0200

    Add Struct Reservatoin

--- Hotel.playground/Contents.swift
+++ Hotel.playground/Contents.swift
@@ -17,16 +17,36 @@ struct Client: Equatable, Hashable {
     
 }
 
+// Estructura Reserva
+
+struct Reservation {
+ 
+    let id: Int
+    // The ID coud be a var that add 1 every time that one reservation is made
+    // somthing like: var idNumber = 0 // and at the end of func addreservation
+    // idNumber += 1
+    let hotelName: String
+    let clientList: Array<Any>
+    let stayInDays: Int
+    let price: Double
+    let breakfast: Bool
+    
+}
+
+
+
+// Pruebas para eliminar
+
 let Goku = Client(name: "Goku", age: 45, height: 1.95)
 Goku.name
 Goku.age
 Goku.height
 let Vegeta = Client(name: "Vegeta", age: 48, height: 1.80)
-
 var people: [Any] = []
-
 people.append(Goku)
-
 people.append(Vegeta)
-
 people
+
+let TeamHuman = Reservation(id: 0, hotelName: "Sky", clientList: ["Goku","Vegetta","Piccolo"], stayInDays: 5, price: 25.50, breakfast: true)
+
+TeamHuman
